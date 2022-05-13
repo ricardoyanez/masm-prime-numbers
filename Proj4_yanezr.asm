1eab7821f069c3d5b5f9cfa18a37b08d75e1993b
Ricardo Yanez <yanezr@oregonstate.edu>
2022-05-13 16:58:32 -0700

    more comments and guideline compliance

--- Proj4_yanezr.asm
+++ Proj4_yanezr.asm
@@ -1,4 +1,4 @@
-TITLE Primes     (Proj4_yanezr.asm)
+TITLE Display Prime Numbers     (Proj4_yanezr.asm)
 
 ; Author: Ricardo Yanez
 ; Last Modified: 5/15/2022
@@ -28,48 +28,59 @@ TRUE = 1
   prompt1		BYTE	"Enter the number of primes to display [",0
   prompt2		BYTE	"]: ",0
 
-  dots			BYTE	" ... ",0
+  dash			BYTE	"-",0
   space			BYTE	"   ",0
 
   num			DWORD	?			; number of primes
   mum			DWORD	?
   mun			DWORD	?
-  lun			DWORD	0           ; count of primes
-  bprime		DWORD	?           ; isPrime boolean
+  lun			DWORD	0           ; count primes
+  bprime		DWORD	?           ; prime boolean
   bvalid		DWORD	?           ; validate boolean
 
   invalid		BYTE	"No primes for you! Number out of range. Try again.",13,10,0
 
-  prompt3		BYTE	"Results certified by Ricardo Yanez. Goodbye.",13,10,0
+  prompt3		BYTE	"Results certified by Ricardo. Goodbye.",13,10,0
 
 .code
 main PROC
 
-  CALL introduction
-  CALL getUserData
-  CALL showPrimes
-  CALL farewell
+  CALL introduction		; Display greeting and instructions
+  CALL getUserData		; Get a value and validate 
+  CALL showPrimes		; Display the prime numbers
+  CALL farewell			; Display end credits
 
   Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
 
-;------------------------------------------------------;
-; Name: introduction                                   ;
-;                                                      ;
-; Display program title and instructions.              ;
-;                                                      ;
-; Preconditions: prompt string must have been defined. ;
-;------------------------------------------------------;
+;-------------------------------------------------------;
+; Name: introduction                                    ;
+;                                                       ;
+; Display program title and instructions.               ;
+;                                                       ;
+; Preconditions: prompt strings must have been defined. ;
+;-------------------------------------------------------;
   introduction PROC
+
 	; preserve registers
 	PUSH EDX
 
-	MOV EDX, OFFSET greeting	; display greeting
+	;-----------------
+	; display greeting
+	;-----------------
+	MOV EDX, OFFSET greeting
+	CALL WriteString
+
+	;---------------------
+	; display instructions
+	;---------------------
+	MOV EDX, OFFSET instruct
 	CALL WriteString
 
 	; restore registers
 	POP EDX
+
 	RET
   introduction ENDP
 
@@ -81,58 +92,58 @@ main ENDP
 ;                                                      ;
 ; Preconditions: prompt string must have been defined. ;
 ;                                                      ;
-; Postconditions: num is within the range.             ;
+; Postconditions: input num is within the range.       ;
 ; -----------------------------------------------------;
   getUserData PROC
 
-	; preserve registers
-	PUSH EDX
-
-	MOV EDX, OFFSET instruct	; display instructions
-	CALL WriteString
-
-_start:
+	;--------------------------
+	; get a value and validate
+	; if not valid, start again
+	;--------------------------
+  _loop:
 	CALL getValue
 	CALL validate
 	CMP bvalid, FALSE
 	JG _continue
-	JMP _start
-_continue:
-
-	; restore registers
-	POP EDX
+	JMP _loop
+  _continue:
 
 	RET
   getUserData ENDP
 
-;------------------------------------------------------;
-; Name: getValue                                       ;
-;                                                      ;
-; Prompt and read for a user supplied value.           ;
-;                                                      ;
-; Preconditions: prompt string must have been defined. ;
-;                                                      ;
-; Returns: num, the user input.                        ;
-;------------------------------------------------------;
+
+;-------------------------------------------------------;
+; Name: getValue                                        ;
+;                                                       ;
+; Prompt and read a user supplied value.                ;
+;                                                       ;
+; Preconditions: prompt strings must have been defined. ;
+;                                                       ;
+; Returns: num, the user input.                         ;
+;-------------------------------------------------------;
   getValue PROC
 
 	; preserve registers
 	PUSH EAX
 	PUSH EDX
 
+	;---------------------------
 	; display prompt with limits
+	; --------------------------
 	MOV EDX, OFFSET prompt1
 	CALL WriteString
 	MOV EAX, LO
 	CALL WriteDec
-	MOV EDX, OFFSET dots
+	MOV EDX, OFFSET dash
 	CALL WriteString
 	MOV EAX, HI
 	CALL WriteDec
 	MOV EDX, OFFSET prompt2
 	CALL WriteString
 
+	;---------------
 	; read the value
+	;---------------
 	CALL ReadDec
 	MOV num, EAX
 
@@ -154,20 +165,26 @@ _continue:
 ;                                                              ;
 ; Receives: num, the number of primes to display               ;
 ;                                                              ;
-; Returns: Boloean bvalid, 1 if within the range, 0 otherwise. ;
+; Returns: Boolean bvalid, 1 if within the range, 0 otherwise. ;
 ;--------------------------------------------------------------;
   validate PROC
 
 	; preserve registers
 	PUSH EDX
 
+	; ---------------------------------
+	; check if num is withing the range
+	; ---------------------------------
 	MOV bvalid, TRUE
 	CMP num, LO
 	JL _invalid
 	CMP num, HI
 	JLE _valid
 
+	; ----------------------
 	; number is not in range
+	; display error
+	;-----------------------
   _invalid:
 	MOV EDX, OFFSET invalid
 	CALL WriteString
@@ -196,13 +213,22 @@ _continue:
 	; preserve registers
 	PUSH EAX
 	PUSH EBX
-    PUSH ECX
+	PUSH ECX
 	PUSH EDX
 
+	;-----------------------------
+	; define loop couter and reset
+	; the other counters
+	;-----------------------------
 	MOV ECX, num
 	MOV mum, 0
 	MOV lun, 0
 
+	;----------------------------
+	; loop and check if tentative
+	; number is a prime
+	; it it is, continue
+	;----------------------------
   _loop:
 	INC mum
 	CALL isPrime
@@ -210,14 +236,20 @@ _continue:
 	CMP EBX, 0
 	JE _loop
 
+	;----------------------------
 	; here we have a prime number
+	; display the number
+	;----------------------------
 	INC lun
 	MOV EAX, mum
 	CALL WriteDec
 	MOV EDX, OFFSET space
 	CALL WriteString
 
-	; if display 10 prime numbers, new line
+	;--------------------------------------
+	; if multiple of 10 prime numbers
+	; have been displayed, issue a new line
+	;--------------------------------------
 	MOV EDX, 0
 	MOV EAX, lun
 	MOV EBX, 10
@@ -229,11 +261,14 @@ _continue:
 	LOOP _loop
 	CALL CrLf
 
+	;--------------------------------------
 	; extra line if not starting a new line
+	;--------------------------------------
 	CMP EDX, 0
-	JE _next1
+	JE _done
 	CALL CrLf
-  _next1:
+
+  _done:
 
 	; restore registers
 	POP EDX
@@ -245,16 +280,17 @@ _continue:
   showPrimes ENDP
 
 
-;---------------------------------------------------------------------------;
-; Name: isPrime                                                             ;
-;                                                                           ;
-; Determines if a number is a prime                                         ;
-;                                                                           ;
-; Receives: the global variable mum must hold the prime number.             ;
-;                                                                           ;
-; Returns: the global variable bprime is 1 if number is prime, 0 otherwise. ;
-;---------------------------------------------------------------------------;
+;----------------------------------------------------------------------------;
+; Name: isPrime                                                              ;
+;                                                                            ;
+; Determines if a number is a prime                                          ;
+;                                                                            ;
+; Receives: the global variable mum must hold the prime number.              ;
+;                                                                            ;
+; Returns: the boolean variable bprime is 1 if number is prime, 0 otherwise. ;
+;----------------------------------------------------------------------------;
   isPrime PROC
+
 	; preserve registers
 	PUSH EAX
 	PUSH EBX
@@ -262,26 +298,42 @@ _continue:
 	PUSH EDX
 
 	MOV bprime, FALSE
-	CMP mum, 1          ; 1 is not a prime number
+
+	;-----------------------
+	; 1 and 2 are base cases
+	;-----------------------
+	CMP mum, 1		; 1 is not a prime number
 	JE _not_prime
-	CMP mum, 2          ; 2 is a prime number
+	CMP mum, 2      ; 2 is a prime number
 	JE _is_prime
-    MOV	mun, 2
-	; loop from 2 to mum/2
+	MOV	mun, 2
+
+	;-----------------------------------------------
+	; loop mun from 2 to half the number
+	; and test if number is divisible by mun.
+	; if it is, then number cannot be a prime number
+	;-----------------------------------------------
   _loop:
 	MOV EDX, 0
 	MOV EAX, mum
 	DIV mun
 	CMP EDX, 0
-	; if mum % mun is zero, mum is divisable and not a prime
+	;------------------------------------
+	; if mum % mun == 0, mum is divisible
+	; by mun and therefore not a prime
+	;------------------------------------
 	JE _not_prime
 	MOV EAX, mun
 	MOV EBX, 2
 	MUL EBX
 	CMP EAX, mum
 	JG _is_prime
-    INC mun
+	INC mun
 	JNE _loop
+
+	;----------------
+	; number is prime
+	;----------------
   _is_prime:
 	MOV bprime, TRUE
 
@@ -292,6 +344,7 @@ _continue:
 	POP ECX
 	POP EBX
 	POP EAX
+
     RET
   isPrime ENDP
 
@@ -304,14 +357,19 @@ _continue:
 ; Preconditions: prompt string must have been defined. ;
 ;------------------------------------------------------;
   farewell PROC
+
 	; preserve registers
 	PUSH EDX
 
+	;--------------------
+	; display end credits
+	;--------------------
 	MOV EDX, OFFSET prompt3
 	CALL WriteString
 
 	; restore registers
 	POP EDX
+
 	RET
   farewell ENDP
 
